e0b72f7e17459909889575dbd217ff12a890c8db
Arthur <abasov.ne@gmail.com>
2023-08-30 21:12:46 +0200

    style mit grid NavMenu

--- frontend/app/src/components/NavMenu/index.jsx
+++ frontend/app/src/components/NavMenu/index.jsx
@@ -7,20 +7,21 @@ import s from './style.module.css'
 export default function NavMenu() {
   return (
     <div className={s.nav_head}>
+
       <img className={s.logo} src={logo} alt="logo Garten" />
 
-      <div className={s.nav_menu}>
-        <Link to='/categories'>
-          <button>Catalog</button>
-        </Link>
+      <Link to='/categories'>
+        <button>Catalog</button>
+      </Link>
 
-        <div className={s.nav_page}>
-          <Link to='/' >Main Page</Link>
-          <Link to='/products' >All products</Link>
-          <Link to='/sale'>All sales</Link>
-        </div>
+      <div className={s.nav_page}>
+        <Link to='/' >Main Page</Link>
+        <Link to='/products' >All products</Link>
+        <Link to='/sale'>All sales</Link>
       </div>
+
       <img className={s.cart} src={cart} alt="Cart" />
+      
     </div>
   )
 }
