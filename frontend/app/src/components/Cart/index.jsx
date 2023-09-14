2e4a9b350482d492271381047d90ddd28a4137cf
Arthur <abasov.ne@gmail.com>
2023-09-14 18:26:00 +0200

    style Cart, next make filter & sort

--- frontend/app/src/components/Cart/index.jsx
+++ frontend/app/src/components/Cart/index.jsx
@@ -9,21 +9,55 @@ export default function Cart({ h1 }) {
 
     const cart_state = useSelector(state => state.cart)
     const dispatch = useDispatch()
-    // console.log(cart_state);
+
+    const total = cart_state.reduce((acc, { price, count }) => acc + price * count, 0).toFixed(2).toString().split('.');
 
     return (
         <div>
             <h1>{h1}</h1>
-            <div className={s.container_cart}>
-                {cart_state.map(el =>
-                    <CartItem key={el.id} {...el} />)}
-            </div>
-            <div
-                className={s.clear_btn}
-                onClick={()=>dispatch(clearCartAction())}
-            >
-                Clear cart
+            <div className={s.container}>
+
+                {cart_state.length === 0
+                    ? (<p>Корзина пуста, товаров нет</p>)
+                    : (<>
+                        <div className={s.container_cart}>
+                            {cart_state.map(el =>
+                                <CartItem key={el.id} {...el} />)}
+                        </div>
+                    </>)
+                }
+                <div className={s.details}>
+                    <h3>Order details</h3>
+                    <div className={s.total_price}>
+                        <p>Total price</p>
+                        <p>
+                            <span>{total[0]}</span>
+                            <span>.{total[1]}</span>
+                            <span> $</span>
+                        </p>
+                    </div>
+                    <form className={s.details_form}>
+                        <input type='number' placeholder='Phone number' />
+                        <button>Order</button>
+                    </form>
+                </div>
+
             </div>
         </div>
     )
 }
+
+/* 
+!!! Очистить корзину:
+
+<div
+    className={s.clear_btn}
+    onClick={()=>dispatch(clearCartAction())}
+>
+    Clear cart
+</div> 
+
+*/
+
+
+
--- frontend/app/src/components/CartItem/index.jsx
+++ frontend/app/src/components/CartItem/index.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import s from './style.module.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { decremProdAction, deleteFromCardAction, incremProdAction } from '../../store/redusers/cartReducer'
+import { RxCross2 } from "react-icons/rx"
 
 export default function CartItem({ count, id, title, image, description, discont_price, price }) {
 
     const dispatch = useDispatch()
-    const cart_state = useSelector(state => state.cart)
-
-
-    const total = cart_state.reduce((acc, { price, count }) => acc + price * count, 0);
+    const price_fix = (price * count).toFixed(2).toString().split('.')
 
     return (
         <div className={s.container}>
@@ -17,30 +15,34 @@ export default function CartItem({ count, id, title, image, description, discont
                 <img src={`http://localhost:3333${image}`} alt={title} />
 
                 <div className={s.products_descr}>
-                    <p>{title}</p>
+                    
+                    <h3>{title}</h3>
+
                     <div className={s.count}>
-                        <button onClick={() => dispatch(incremProdAction(id))}>+</button>
+                        <div onClick={() => dispatch(decremProdAction(id))}>-</div>
                         <p>{count}</p>
-                        <button onClick={() => dispatch(decremProdAction(id))}>-</button>
+                        <div onClick={() => dispatch(incremProdAction(id))}>+</div>
                     </div>
 
                 </div>
-                <p>{price * count}$</p>
 
                 <div className={s.price}>
-                    <h2>{price}$</h2>
+
+                    <h2>
+                        <span>{price_fix[0]}</span>
+                        <span>.{price_fix[1]}</span>
+                        <span> $</span>
+                    </h2>
+
                     {discont_price ?
                         <p className={s.discont}>{Math.floor(price - price * discont_price / 100)}$</p> : ''}
-                    {discont_price ?
-                        <p className={s.prozent}>-{discont_price}%</p> : ''}
                 </div>
 
-                <span onClick={() => dispatch(deleteFromCardAction(id))}>X</span>
+                <span onClick={() => dispatch(deleteFromCardAction(id))}>
+                    <RxCross2/>
+                </span>
             </div>
-            <div className={s.details}>
-                <p>Total price</p>
-                <p>{total}</p>
-            </div>
+            
         </div>
     )
 }
