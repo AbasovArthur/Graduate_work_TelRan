576f388c63051e0ca19db46653e10ef5d6c5ee7b
Arthur <abasov.ne@gmail.com>
2023-09-20 16:18:15 +0200

    stule DiscountProductForm & make toggler btn for discont

--- frontend/app/src/components/FilterForms/DiscountProductForm/index.jsx
+++ frontend/app/src/components/FilterForms/DiscountProductForm/index.jsx
@@ -1,23 +1,44 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { getDiscountPropuctsAction } from '../../../store/redusers/allProductsReduser'
+import s from './style.module.css'
+
 
 export default function DiscountProductForm() {
 
-    const dicpatch = useDispatch()
+  const dicpatch = useDispatch()
 
-    const [checkDiscount, setCheckDiscount] = useState(false)
+  const [checkDiscount, setCheckDiscount] = useState(false)
 
-    const handelChange = () => setCheckDiscount(!checkDiscount)
+  const handelChange = () => setCheckDiscount(!checkDiscount)
 
-    const get_products = e => dicpatch(getDiscountPropuctsAction(e.target.checked))
+  const get_products = e => dicpatch(getDiscountPropuctsAction(e.target.checked))
 
   return (
     <div>
-        <label>
-            <p>Discounted items</p>
-            <input type='checkbox' checked={checkDiscount} onChange={handelChange} onClick={get_products}/>
+      <label className={s.discount}>
+        <p>Discounted items</p>
+        {/* <span>{checkDiscount ? '✔' : '✖'}</span> */}
+
+        <label
+          className={checkDiscount ? `${s.toggler} ${s.active}` : s.toggler}
+          htmlFor='checkbox'
+        >
+          <input
+            type='checkbox'
+            id='checkbox'
+            checked={checkDiscount}
+            onChange={handelChange}
+            onClick={get_products}
+          />
+          <span className={s.ball}>
+            {/* {checkDiscount ? '✔' : ''} */}
+            {''}
+          </span>
         </label>
+
+      </label>
     </div>
   )
+
 }
--- frontend/app/src/store/redusers/productsByCategoriesReducer.js
+++ frontend/app/src/store/redusers/productsByCategoriesReducer.js
@@ -5,8 +5,11 @@ export const loadProductByCategory = payload => ({type: LOAD_PRODUCT_BY_CATEGORY
 export const productsByCategoriesReducer = (state = {}, action) => {
     if (action.type === LOAD_PRODUCT_BY_CATEGORY) {
         const { data, category } = action.payload;
+        
         // return {...state, category, data, show_product: true }
-        return {...state, category, data }
+
+        const data_2 = data.map(el => ({...el, show_product: true}));
+        return {...state, category, data: data_2 }
     } else {
         return state
     }
