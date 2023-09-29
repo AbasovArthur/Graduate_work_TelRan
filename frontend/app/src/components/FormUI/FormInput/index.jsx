1cbf386f76cf9681216186cd7f94eef64c81ea75
Arthur <abasov.ne@gmail.com>
2023-09-29 17:16:28 +0200

    make useForm & FormItem phone number validation

--- frontend/app/src/components/FormUI/FormInput/index.jsx
+++ frontend/app/src/components/FormUI/FormInput/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import s from './style.module.css'
 
-export default function FormInput(props) {
+const FormInput = forwardRef((props, ref) => {
   // console.log(props);
   return (
-    <input {...props} className={[s.input, s[props.style_item]].join(' ')}/>
+    <input {...props}
+      className={[s.input, s[props.style_item]].join(' ')}
+      ref={ref}
+    />
   )
-}
+})
+
+export default FormInput;
--- frontend/app/src/components/FormUI/FormItem/index.jsx
+++ frontend/app/src/components/FormUI/FormItem/index.jsx
@@ -1,20 +1,43 @@
-//frontend\app\src\components\FormItem\index.jsx
 import React from 'react'
 import FormButton from '../FormButton'
 import FormInput from '../FormInput'
 import s from './style.module.css'
+import { useForm } from 'react-hook-form'
 
 export default function FormItem(props) {
 
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({
+    // mode: 'onChange'
+  });
+
+  const numRegister = register('mob_number', {
+    required: '*This field is required',
+    pattern: {
+      value: /^(?:\+49|0049|0)?\s?\d{2,5}\s?\d{4,8}$/,
+      message: "*Oops! That doesn't look like a valid phone number. Please try again."
+    }
+  })
+
+  const submit = data => {
+    console.log(data)
+    reset()
+  };
+
   return (
-    <div className={[s.form_item, s[props.style_item]].join(' ')}>
-      <FormInput
-        type='number'
-        placeholder='+49'
-        name='mob_number'
-        style_item={props.input_style_item}
-      />
-      <FormButton btn_style_item={props.btn_style_item}>{props.title_btn}</FormButton>
-    </div>
+    <form onSubmit={handleSubmit(submit)}>
+
+      <div className={[s.form_item, s[props.style_item]].join(' ')}>
+        <FormInput
+          type='tel'
+          placeholder='+49'
+          name='mob_number'
+          style_item={props.input_style_item}
+          {...numRegister}
+          />
+          {errors.mob_number && 
+            <p className={s.error_msg}>{errors.mob_number.message}</p>}
+        <FormButton btn_style_item={props.btn_style_item}>{props.title_btn}</FormButton>
+      </div>
+    </form>
   )
 }
