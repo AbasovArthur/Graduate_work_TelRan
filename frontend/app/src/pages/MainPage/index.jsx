cd6f80731691c23b640a3a28fdcee55f3d764009
Arthur <abasov.ne@gmail.com>
2023-09-30 18:12:45 +0200

    make random ProductContainer & CategoriesContainer in MainPage

--- frontend/app/src/pages/MainPage/index.jsx
+++ frontend/app/src/pages/MainPage/index.jsx
@@ -30,26 +30,34 @@ export default function MainPage() {
     };
   }, [ limit ]);
 
+  const products_state = useSelector(state => state.allProducts)
+  const category_state = useSelector(state => state.categories)
 
-  const all_products_state = useSelector(state => state.allProducts)
-  const products_like_sale = all_products_state.filter(el=>el.discont_price)
-  
-  const show_product_sale = visibleCard ? (products_like_sale).slice(0, visibleCard) : products_like_sale
-  
-  const category_store = useSelector(state => state.categories)
-  const show_category = visibleCard ? category_store.slice(0, visibleCard) : category_store
+  const shuffleArray = (array) => {
+    return array.reduce((acc, value) => {
+      const randIndex = Math.floor(Math.random() * (acc.length + 1));
+      return [...acc.slice(0, randIndex), value, ...acc.slice(randIndex)];
+    }, []);
+  }
+
+  const products_like_sale = shuffleArray(products_state.filter(el => el.discont_price)).slice(0, visibleCard);
+  const show_category = shuffleArray(category_state).slice(0, visibleCard);
+  // const show_category = visibleCard ? category_state.slice(0, visibleCard) : category_state
 
 
   return (
     <div>
       <MainBanner />
       <CategoriesContainer categories={show_category} h1={'Catalog'} />
-      {/* <CategoriesContainer categories={show_product_sale} h1={'Catalog'} /> */}
       <MainForm />
-      <ProductsContainer products={show_product_sale} h1={'Sale'} />
+      <ProductsContainer products={products_like_sale} h1={'Sale'} />
     </div>
   )
 }
+
+  // const products_like_sale = shuffleArray(products_state.filter(el => el.discont_price));
+  
+  // const show_product_sale = visibleCard ? (products_like_sale).slice(0, visibleCard) : products_like_sale
 /*
   const all_products_state = useSelector(state => state.allProducts)
   const products_like_sale = all_products_state.filter(el=>el.discont_price)
@@ -68,3 +76,10 @@ export default function MainPage() {
   
   const show_product_sale = visibleCard ? (products_like_sale || category_store).slice(0, visibleCard) : (products_like_sale || category_store)
   */
+
+  /*
+  const { products_state, category_state } = useSelector(state => ({
+    products_state: state.allProducts,
+    category_state: state.categories
+  }));
+*/
