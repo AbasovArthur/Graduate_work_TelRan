53491e6c52a2ec6e706dada91b9b85a968efaa3a
Arthur <abasov.ne@gmail.com>
2023-10-14 16:21:26 +0200

    display SingleProductPage

--- frontend/my_app/src/components/ProductPrice/index.jsx
+++ frontend/my_app/src/components/ProductPrice/index.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import s from './style.module.css'
 import { useSelector } from 'react-redux'
 
-export default function ProductPrice({ price, discont_price }) {
+export default function ProductPrice({ price, discont_price, style_single_page  }) {
 
+    // const style_price = () => {
+    //     [s.price, style_single_page].join(' ')
+    // }
+    console.log("Received style:", style_single_page);
 
     return (
-        <div className={s.price}>
+        // <div className={style_price}>
+        <div className={[s.price, s[style_single_page]].join(' ')}>
             {discont_price ?
                 <>
                     <h2>{discont_price}$</h2>
--- frontend/my_app/src/pages/SingleProductPage/index.jsx
+++ frontend/my_app/src/pages/SingleProductPage/index.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import s from './style.module.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import ProductPrice from '../../components/ProductPrice';
 
 export default function SingleProductPage() {
+
+  const { id } = useParams();
+
+  const single_product_state = useSelector( state => state.allProducts )
+
+  // console.log(single_product_state);
+
+  const product = single_product_state[id -1]
+
+  const {
+    title,
+    image,
+    price,
+    discont_price,
+    description,
+    updatedAt,
+    categoryId,
+    createdAt
+  } = product || {};
+
   return (
-    <div>SingleProductPage</div>
+    <div>
+    <h3 className={s.title}>{title}</h3>
+    <div className={s.container_card}>
+      {image &&
+        <img className={s.img} src={`http://localhost:3333${image}`} alt={title} />
+      }
+      <div className={s.card_descr}>
+
+          <ProductPrice price={price} discont_price={discont_price} style_single_page='style_single_page'/>
+
+        <div className={s.title_descr}>
+          <p>Description</p>
+          <p>{description}</p>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
   )
 }
+
+
