2da52059a4574c9c04f84de7432224035e85ddfc
Arthur <abasov.ne@gmail.com>
2023-10-15 12:31:58 +0200

    display AllSalesPage

--- frontend/my_app/src/pages/AllSalesPage/index.jsx
+++ frontend/my_app/src/pages/AllSalesPage/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import DiscFiltrSortForm from '../../components/FilterForms/DiscFiltrSortForm'
+import ProductsContainer from '../../components/ProductsContainer'
+import { useSelector } from 'react-redux'
 
 export default function AllSalesPage() {
+
+  const all_products_state = useSelector(state => state.allProducts)
+  const products_like_sale = all_products_state.filter(el => el.discont_price)
+
+
   return (
-    <div>AllSalesPage</div>
+    <div>
+      <DiscFiltrSortForm discount_show={false}/>
+      <ProductsContainer products={products_like_sale} h1={'Products with sale'} />
+    </div>
   )
 }
